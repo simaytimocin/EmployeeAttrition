80841f3bfabfa0948934d8eb2290cf51d1f25679
E. Simay Timoçin <93477058+simaytimocin@users.noreply.github.com>
2024-11-19 15:06:30 +0300

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def attrition():
         
         age = st.number_input("Age", min_value=18, max_value=80, help="18-80")
         business_travel = st.radio("Business Travel", ["Rarely", "Frequently", "No Travel"])
-        daily_rate = st.number_input("Daily Rate", min_value=100, max_value=1600, help="100-1600")
         department = st.radio("Department", ["Research & Development", "Human Resources", "Sales"])
         distance_from_home = st.number_input("Distance From Home", min_value=1, max_value=29, help="1-29")
         education = st.radio("Education", [1, 2, 3, 4, 5])
@@ -26,7 +25,6 @@ def attrition():
             "Life Sciences", "Medical", "Marketing", "Technical Degree", "Human Resources", "Other"])
         environment_satisfaction = st.radio("Environment Satisfaction", [1, 2, 3, 4])
         gender = st.radio("Gender", ["Male", "Female"])
-        hourly_rate = st.number_input("Hourly Rate", min_value=30, max_value=100, help="30-100")
         job_involvement = st.number_input("Job Involvement", min_value=1, max_value=4, help="1-4")
         job_level = st.number_input("Job Level", min_value=1, max_value=5, help="1-5")
         job_role = st.selectbox("Job Role", [
@@ -40,7 +38,6 @@ def attrition():
         overtime = st.radio("Over Time", ["Yes", "No"])
         performance_rating = st.number_input("Performance Rating", min_value=1, max_value=4, help="1-4")
         relationship_satisfaction = st.number_input("Relationship Satisfaction", min_value=1, max_value=4, help="1-4")
-        stock_option_level = st.selectbox("Stock Option Level", [0, 1, 2, 3])
         total_working_years = st.number_input("Total Working Years", min_value=0, max_value=40, help="0-40")
         training_times_last_year = st.number_input("Training Times Last Year", min_value=0, max_value=6, help="0-6")
         work_life_balance = st.number_input("Work Life Balance", min_value=1, max_value=4, help="1-4")
@@ -56,14 +53,12 @@ def attrition():
         input_data = {
             "Age": age,
             "BusinessTravel": business_travel,
-            "DailyRate": daily_rate,
             "Department": department,
             "DistanceFromHome": distance_from_home,
             "Education": education,
             "EducationField": education_field,
             "EnvironmentSatisfaction": environment_satisfaction,
             "Gender": gender,
-            "HourlyRate": hourly_rate,
             "JobInvolvement": job_involvement,
             "JobLevel": job_level,
             "JobRole": job_role,
@@ -74,7 +69,6 @@ def attrition():
             "OverTime": overtime,
             "PerformanceRating": performance_rating,
             "RelationshipSatisfaction": relationship_satisfaction,
-            "StockOptionLevel": stock_option_level,
             "TotalWorkingYears": total_working_years,
             "TrainingTimesLastYear": training_times_last_year,
             "WorkLifeBalance": work_life_balance,
