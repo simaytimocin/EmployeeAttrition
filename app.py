9d51168de59d5c810dfe62790810cd0545e9a410
E. Simay Timoçin <93477058+simaytimocin@users.noreply.github.com>
2024-11-19 17:03:19 +0300

    app.py

--- app.py
+++ app.py
@@ -9,47 +9,47 @@ scaler = pickle.load(open("mm_scaler.pkl", 'rb'))
 features = pickle.load(open("xgb_features.pkl", 'rb'))
 
 def attrition():
-    st.title("Employee Attrition Prediction App")
+    st.set_page_config(page_title="Çalışan Kayıp Tahmin Uygulaması", page_icon="📈", layout="centered")
+    st.title("Çalışan Kayıp Tahmin Uygulaması")
 
     with st.form("attrition_form"):
 
-        st.header("Employee Details")
+        st.header("Çalışan Bilgileri")
 
         # Input fields for user to provide employee details
-        age = st.number_input("Age", min_value=18, max_value=80, help="18-80")
-        business_travel = st.radio("Business Travel", ["Rarely", "Frequently", "No Travel"])
-        daily_rate = st.number_input("Daily Rate", min_value=100, max_value=1600, help="100-1600")
-        department = st.radio("Department", ["Research & Development", "Human Resources", "Sales"])
-        distance_from_home = st.number_input("Distance From Home", min_value=1, max_value=29, help="1-29")
-        education = st.radio("Education", [1, 2, 3, 4, 5])
-        education_field = st.selectbox("Education Field", [
-            "Life Sciences", "Medical", "Marketing", "Technical Degree", "Human Resources", "Other"])
-        environment_satisfaction = st.radio("Environment Satisfaction", [1, 2, 3, 4])
-        gender = st.radio("Gender", ["Male", "Female"])
-        hourly_rate = st.number_input("Hourly Rate", min_value=30, max_value=100, help="30-100")
-        job_involvement = st.number_input("Job Involvement", min_value=1, max_value=4, help="1-4")
-        job_level = st.number_input("Job Level", min_value=1, max_value=5, help="1-5")
-        job_role = st.selectbox("Job Role", [
-            "Sales Executive", "Research Scientist", "Laboratory Technician", "Manufacturing Director",
-            "Healthcare Representative", "Manager", "Sales Representative", "Research Director",
-            "Human Resources"])
-        job_satisfaction = st.radio("Job Satisfaction", [1, 2, 3, 4])
-        marital_status = st.selectbox("Marital Status", ["Married", "Single", "Divorced"])
-        monthly_income = st.number_input("Monthly Income", min_value=1000, max_value=20000, help="1000-20000")
-        num_companies_worked_in = st.number_input("No of Companies Worked in", min_value=0, max_value=9, help="0-9")
-        overtime = st.radio("Over Time", ["Yes", "No"])
-        performance_rating = st.number_input("Performance Rating", min_value=1, max_value=4, help="1-4")
-        relationship_satisfaction = st.number_input("Relationship Satisfaction", min_value=1, max_value=4, help="1-4")
-        stock_option_level = st.selectbox("Stock Option Level", [0, 1, 2, 3])
-        total_working_years = st.number_input("Total Working Years", min_value=0, max_value=40, help="0-40")
-        training_times_last_year = st.number_input("Training Times Last Year", min_value=0, max_value=6, help="0-6")
-        work_life_balance = st.number_input("Work Life Balance", min_value=1, max_value=4, help="1-4")
-        years_at_company = st.number_input("Years at Company", min_value=0, max_value=40, help="0-40")
-        years_in_current_role = st.number_input("Years in Current Role", min_value=0, max_value=18, help="0-18")
-        years_since_last_promotion = st.number_input("Years Since Last Promotion", min_value=0, max_value=15, help="0-15")
-        years_with_curr_manager = st.number_input("Years With Current Manager", min_value=0, max_value=17, help="0-17")
+        age = st.number_input("Yaş", min_value=18, max_value=80, help="18-80")
+        business_travel = st.radio("İş Seyahati Durumu", ["Nadiren", "Sık Sık", "Seyahat Yok"])
+        daily_rate = st.number_input("Günlük Ücret", min_value=100, max_value=1600, help="100-1600")
+        department = st.radio("Departman", ["Araştırma ve Geliştirme", "İnsan Kaynakları", "Satış"])
+        distance_from_home = st.number_input("Evden Uzaklık (km)", min_value=1, max_value=29, help="1-29")
+        education = st.radio("Eğitim Seviyesi", [1, 2, 3, 4, 5])
+        education_field = st.selectbox("Eğitim Alanı", [
+            "Fen Bilimleri", "Tıp", "Pazarlama", "Mühendislik Derecesi", "İnsan Kaynakları", "Diğer"])
+        environment_satisfaction = st.radio("Çevre Memnuniyeti", [1, 2, 3, 4])
+        gender = st.radio("Cinsiyet", ["Erkek", "Kadın"])
+        hourly_rate = st.number_input("Saatlik Ücret", min_value=30, max_value=100, help="30-100")
+        job_involvement = st.number_input("İşe Katılım", min_value=1, max_value=4, help="1-4")
+        job_level = st.number_input("İş Seviyesi", min_value=1, max_value=5, help="1-5")
+        job_role = st.selectbox("İş Rolü", [
+            "Satış Yöneticisi", "Araştırma Bilimcisi", "Laboratuvar Teknisyeni", "Üretim Müdürü",
+            "Sağlık Temsilcisi", "Müdür", "Satış Temsilcisi", "Araştırma Müdürü", "İnsan Kaynakları"])
+        job_satisfaction = st.radio("İş Memnuniyeti", [1, 2, 3, 4])
+        marital_status = st.selectbox("Medeni Durum", ["Evli", "Bekar", "Boşanmış"])
+        monthly_income = st.number_input("Aylık Gelir", min_value=1000, max_value=20000, help="1000-20000")
+        num_companies_worked_in = st.number_input("Çalışılan Şirket Sayısı", min_value=0, max_value=9, help="0-9")
+        overtime = st.radio("Fazla Mesai", ["Evet", "Hayır"])
+        performance_rating = st.number_input("Performans Değerlendirmesi", min_value=1, max_value=4, help="1-4")
+        relationship_satisfaction = st.number_input("İlişki Memnuniyeti", min_value=1, max_value=4, help="1-4")
+        stock_option_level = st.selectbox("Hisse Seçenek Seviyesi", [0, 1, 2, 3])
+        total_working_years = st.number_input("Toplam Çalışma Yılları", min_value=0, max_value=40, help="0-40")
+        training_times_last_year = st.number_input("Geçen Yıl Eğitim Sayısı", min_value=0, max_value=6, help="0-6")
+        work_life_balance = st.number_input("İş-Yaşam Dengesi", min_value=1, max_value=4, help="1-4")
+        years_at_company = st.number_input("Şirkette Geçen Yıl", min_value=0, max_value=40, help="0-40")
+        years_in_current_role = st.number_input("Mevcut Rolde Geçen Yıl", min_value=0, max_value=18, help="0-18")
+        years_since_last_promotion = st.number_input("Son Terfiden Beri Geçen Yıl", min_value=0, max_value=15, help="0-15")
+        years_with_curr_manager = st.number_input("Mevcut Yöneticiyle Geçen Yıl", min_value=0, max_value=17, help="0-17")
 
-        submitted = st.form_submit_button("Predict Attrition")
+        submitted = st.form_submit_button("Çalışan Kayıp Tahmini Yap")
 
     if submitted:
         # Create DataFrame from input data
@@ -109,9 +109,9 @@ def attrition():
 
         # Display prediction result
         if prediction[0] == 1:
-            st.write("Attrition Predicted: Yes")
+            st.write("Kayıp Tahmini: Evet 🙁")
         else:
-            st.write("Attrition Predicted: No")
+            st.write("Kayıp Tahmini: Hayır 🙂")
 
 # Run the app
 attrition()
